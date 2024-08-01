c013f2de0ae12be1c0679cb883ec27fa43772a76
Ed Pino <maopino82@gmail.com>
2024-07-31 22:12:37 -0500

    up load to my branch

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,11 +4,11 @@ import ProductItem from './ProductItem';
 import './ProductList.css'
 
 const products = [
-    {id:1, nameItem:'Sneakers', price:100000, image: './sc.png'},
-    {id:2, nameItem:'Sneakers', price:100000, image: './sc.png'},
-    {id:3, nameItem:'Sneakers', price:100000, image: './sc.png'},
-    {id:4, nameItem:'Sneakers', price:100000, image: './sc.png'},
-    {id:5, nameItem:'Sneakers', price:100000, image: './sc.png'}
+    {id:1, nameItem:'Adidas Samba', price:280000, image: './sc.png'},
+    {id:2, nameItem:'NikeCourt Zoom Vapor', price:380000, image: './nikecourt-zoom-vapor-roger-federer-air-jordan-3-atmos.jpg'},
+    {id:3, nameItem:'Nike Air Max-97', price:420000, image: './undefeated-nike-air-max-97-olive.jpg'},
+    {id:4, nameItem:'Nike Air heaven', price:380000, image: './th.jpg'},
+    {id:5, nameItem:'Nike Jordan 1', price:350000, image: './air-jordan-1-wheat.jpg'}
 ]
 
 function ProductList() {
